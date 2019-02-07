8b327e5375da97b0a8d462035078f07e5f7529a8
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-07 14:36:01 +0700

    Hashing Users Passwords

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const environment = process.env.NODE_ENV;
-const stage = require('./config')[environment];
+const stage = require('../config')[environment];
 
 //schema map to a collection
 const Schema = mongoose.Schema;
@@ -18,6 +18,24 @@ const userSchema = new Schema({
         type: 'String',
         required: true,
         trim: true
+    }
+});
+
+//encrypt password before save
+userSchema.pre('save', function(next) {
+    const user = this;
+    if(!user.isModified || !user.isNew) {
+        next();
+    } else {
+        bcrypt.hash(user.password, stage.saltingRounds, function(err, hash) {
+            if(err) {
+                console.log('Error hashing password for user', user.name);
+                next(err);
+            } else {
+                user.password = hash;
+                next();
+            }
+        })
     }
 });
 
