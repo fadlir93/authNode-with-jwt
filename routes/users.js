c3a211b69f02ba6bd0176bcac371de89e7cdef15
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-07 14:08:53 +0700

    Route Setup

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const controller = require('../controllers/users');
+module.exports = {
+    add: (req, res) => {
+        return;
+    }
+}
+
+module.exports = (router) => {
+    router.route('/users')
+    .post(controller.add);
+}
